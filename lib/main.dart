85d638bdd2c62bf86d7c230c0b4960fa5a053346
Michael Hendrick <mhendrick@byui.edu>
2023-02-18 09:40:05 -0700

    Updated main.dart for readability

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,11 @@ import 'package:record/record.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const Layered());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class Layered extends StatelessWidget {
+  const Layered({super.key});
 
   // This widget is the root of your application.
   @override
@@ -15,24 +15,16 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Layered',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
+        // Primary app Color
         primarySwatch: Colors.deepOrange,
       ),
-      home: const MyHomePage(title: 'Layered'),
+      home: const MicPage(title: 'Layered'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class MicPage extends StatefulWidget {
+  const MicPage({super.key, required this.title});
 
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
@@ -46,14 +38,22 @@ class MyHomePage extends StatefulWidget {
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<MicPage> createState() => _MicPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MicPageState extends State<MicPage> {
 
   Record myRecording = Record();
+
+  // TODO
+  // Initialize an empty list of recordings, as the program continues we will
+  // add new recordings here, and then remove them when needed
+  List<Record> myRecordings = [];
+
   Timer? timer;
 
+  // TODO
+  // Remove these
   double volume = 0.0;
   // This sets what volume the display will trigger at
   double minVolume = -45.0;
@@ -63,7 +63,7 @@ class _MyHomePageState extends State<MyHomePage> {
         const Duration(milliseconds: 50), (timer) => updateVolume());
   }
 
-  // Get the current volume
+  // Get the current volume - Not necessary for final program
   updateVolume() async {
     Amplitude ampl = await myRecording.getAmplitude();
     if (ampl.current > minVolume) {
@@ -73,15 +73,15 @@ class _MyHomePageState extends State<MyHomePage> {
     }
   }
 
-  // Return the volume on a scale from 0 to maxVolume
+  // Return the volume on a scale from 0 to maxVolume - Not necessary for final program
   int volumeScaleTo(int maxVolume) {
     return (volume * maxVolume).round().abs();
   }
 
-  Future<bool> startRecording() async {
-    if (await myRecording.hasPermission()) {
-      if (!await myRecording.isRecording()) {
-        await myRecording.start();
+  Future<bool> startRecording(recording) async {
+    if (await recording.hasPermission()) {
+      if (!await recording.isRecording()) {
+        await recording.start();
       }
       startTimer();
       return true;
@@ -92,40 +92,18 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
 
     return FutureBuilder(
-        future: startRecording(),
-    builder: (context, AsyncSnapshot<bool> snapshot) {
+      future: startRecording(Record()),
+      builder: (context, AsyncSnapshot<bool> snapshot) {
       return Scaffold(
         appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
+        // Here we take the value from the MicPage object that was created by
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
         ),
         body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
           child: Column(
-            // Column is also a layout widget. It takes a list of children and
-            // arranges them vertically. By default, it sizes itself to fit its
-            // children horizontally, and tries to be as tall as its parent.
-            //
-            // Invoke "debug painting" (press "p" in the console, choose the
-            // "Toggle Debug Paint" action from the Flutter Inspector in Android
-            // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-            // to see the wireframe for each widget.
-            //
-            // Column has various properties to control how it sizes itself and
-            // how it positions its children. Here we use mainAxisAlignment to
-            // center the children vertically; the main axis here is the vertical
-            // axis because Columns are vertical (the cross axis would be
-            // horizontal).
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               Text(
